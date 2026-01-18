ece1f07fb1651b23021245f70b17a4bd3a51b6dc
Saimon Rai <saimon11rai15@gmail.com>
2026-01-18 14:42:58 +0545

    fix: encrypt private key storage with password

--- main.py
+++ main.py
@@ -10,25 +10,27 @@ from cryptography.hazmat.primitives import serialization
 def generate_identity():
     """
     Generates a 2048-bit RSA key pair.
-
-    The private key is the user's secret identity.
-    The public key will be used by others to encrypt data for this user.
     """
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
 
+    # FIXED: Encrypting the private key with a hardcoded password
+    # hardcoded password for locking the private key
+    password = b"l0cker1212"
+
     pem = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
+        # Used BestAvailableEncryption to lock the file
+        encryption_algorithm=serialization.BestAvailableEncryption(password)
     )
 
     with open("private_key.pem", "wb") as f:
         f.write(pem)
 
-    print("Identity generated. Private key saved to 'private_key.pem'.")
+    print("Identity generated securely.")
 
 if __name__ == "__main__":
     generate_identity()
